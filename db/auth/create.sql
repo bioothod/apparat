393589731366ca224dc156d66f791c34d4a3e52d
Evgeniy Polyakov <zbr@ioremap.net>
2016-07-12 17:51:10 +0300

    db: do not set default nulls for username and password, null string can not be primary key in mysql 5.6

--- db/auth/create.sql
+++ db/auth/create.sql
@@ -1,8 +1,8 @@
 USE `apparat.auth`;
 
 CREATE TABLE `users` (
-    `username` VARCHAR(128) NULL DEFAULT NULL,
-    `password` VARCHAR(64) NULL DEFAULT NULL,
+    `username` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
     `realname` VARCHAR(128) NULL DEFAULT NULL,
     `email` VARCHAR(128) NULL DEFAULT NULL,
     `created` DATETIME NULL DEFAULT NULL,
